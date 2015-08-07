c3e931309681ee55d4352a4a8bd0a7ee3cb2762a
Valerio Pipolo <valep27@gmail.com>
2015-08-07 20:42:57 +0200

    Tentative implementation for tokenizer/environment

--- src/lisp/lex.rs
+++ src/lisp/lex.rs
@@ -1,4 +1,25 @@
-pub fn tokenize(input : &str) -> Vec<&str> {
-    let tokens: Vec<&str> = input.split(' ').collect();
-    tokens
+// use regex::Regex;
+
+pub fn tokenize(input : &str) -> Vec<String> {
+    let formatted = format_braces(input);
+    let mut tk : Vec<String> = Vec::new();
+
+    for token in formatted.split(' ') {
+        tk.push(token.to_string());
+    }
+
+    tk
+}
+
+fn format_braces(input : &str) -> String {
+    // let re = Regex::new(r"(?P<left>\S\(\S|?P<right>\S\)\S)");
+    input.replace("(", " ( ").replace(")", " ) ").trim().to_string()
+}
+
+
+#[test]
+fn braces_are_formatted() {
+    let input = "(())";
+    let s = format_braces(input);
+    assert_eq!(s, " ( ( ) ) ");
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod lisp;
 use lisp::env::Env;
 use lisp::lex;
 
+extern crate regex;
 
 fn main() {
     let line = "( var some = ( 1 + 2 ) )";
