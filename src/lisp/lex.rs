f33a50950105b907d99eea10e21d4f3f6595a115
Valerio Pipolo <valep27@gmail.com>
2015-12-29 18:42:26 +0100

    new parser, still in progress

--- src/lisp/lex.rs
+++ src/lisp/lex.rs
@@ -1,9 +1,56 @@
 use lisp::cell::Cell;
 
+// TODO: documentation
+
+pub enum ParseError {
+    UnbalancedParens,
+    UnrecognizedToken,
+    EOFReached,
+}
+
+pub type ParseResult = Result<Cell, ParseError>;
+
+// A Reader that provides a way to peek and read a stream of tokens.
+pub struct Reader {
+    tokens: Vec<String>,
+    position: usize,
+}
+
+impl Reader {
+    pub fn new(tokens: Vec<String>) -> Reader {
+        Reader {
+            tokens: tokens,
+            position: 0,
+        }
+    }
+
+    pub fn new_from_str(text: &str) -> Reader {
+        Reader::new(tokenize(text))
+    }
+
+    pub fn peek(&self) -> Option<&str> {
+        if self.position < self.tokens.len() {
+            None
+        } else {
+            Some(self.tokens[self.position].as_ref())
+        }
+    }
+
+    pub fn read(&mut self) -> Option<&str> {
+        if self.position < self.tokens.len() {
+            None
+        } else {
+            let next_token = self.tokens[self.position].as_ref();
+            self.position += 1;
+            Some(next_token)
+        }
+    }
+}
+
 // Produces a vector of Strings, with no empty ones
 pub fn tokenize(input: &str) -> Vec<String> {
     let formatted = format_braces(input);
-    let mut tokens: Vec<String> = Vec::new();
+    let mut tokens = Vec::new();
 
     for token in formatted.split(' ') {
         let t = token.trim();
@@ -11,7 +58,6 @@ pub fn tokenize(input: &str) -> Vec<String> {
         if t != "" {
             tokens.push(t.to_string());
         }
-        
     }
 
     tokens
@@ -20,6 +66,65 @@ pub fn tokenize(input: &str) -> Vec<String> {
 // Puts spaces befor and after braces
 fn format_braces(input: &str) -> String {
     input.replace("(", " ( ").replace(")", " ) ").trim().to_string()
+}
+
+pub fn parse_form(reader: &Reader) -> ParseResult {
+    if let Some(token) = reader.peek() {
+        match token {
+            "(" => parse_list(reader),
+            _ => parse_atom(reader),
+        }
+    }
+}
+
+pub fn parse_list(reader: &Reader) -> ParseResult {
+    // TODO: check for unbalanced parens (review loop)
+    let mut list: Vec<Cell> = Vec::new();
+    // discard first '('
+    let _ = reader.read();
+
+    while let Some(token) = reader.read() {
+        if token != ")" {
+            match parse_form(reader) {
+                Ok(cell) => list.push(cell),
+                Err(e) => return Err(e), 
+            }
+        } else {
+            break;
+        }
+    }
+
+    Ok(Cell::List(list))
+}
+
+fn parse_atom(reader: &Reader) -> ParseResult {
+    match reader.read() {
+        Some(token) => {
+            match try_parse_number(token) {
+                Some(cell) => Ok(cell),
+                None => Ok(parse_other_values(token)),
+            }
+        }
+        None => Err(ParseError::EOFReached),
+    }
+}
+
+fn try_parse_number(text: &str) -> Option<Cell> {
+    let n_f = text.parse::<f32>();
+
+    match n_f {
+        Ok(number) => Some(Cell::Number(number)),
+        Err(_) => None,
+    }
+}
+
+fn parse_other_values(text: &str) -> Cell {
+    match text {
+        "#t" => Cell::True,
+        "#f" => Cell::False,
+        "nil" => Cell::Nil,
+        _ => Cell::Symbol(text.to_string()),                
+    }
 }
 
 // Consumes a vector of Strings, returning data to be used with eval
@@ -53,21 +158,23 @@ pub fn parse_tree_from_tokens(tokens: &mut Vec<String>) -> Option<Cell> {
     result
 }
 
-// Parses an atomic value (number/string)
-fn parse_atom(input: &str) -> Cell {
-    let n_f = input.parse::<f32>();
+// Parses an atomic value.
+// It first attempts to parse a number (only float for now), then matches against other atomic
+// types.
+// fn parse_atom(input: &str) -> Cell {
+//     let n_f = input.parse::<f32>();
 
-    if n_f.is_err() {
-        match input {
-            "#t" => Cell::True,
-            "#f" => Cell::False,
-            "nil" => Cell::Nil,
-            _ => Cell::Symbol(input.to_string()),
-        }
-    } else {
-        Cell::Number(n_f.unwrap())
-    }
-}
+//     if n_f.is_err() {
+//         match input {
+//             "#t" => Cell::True,
+//             "#f" => Cell::False,
+//             "nil" => Cell::Nil,
+//             _ => Cell::Symbol(input.to_string()),
+//         }
+//     } else {
+//         Cell::Number(n_f.unwrap())
+//     }
+// }
 
 
 #[test]
